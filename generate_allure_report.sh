bba62722b26361ce6c2592a38b6856f67185221a
Muhammad Sultan <muhammad.sultan@paymob.com>
2022-08-24 11:16:44 +0000

    Delete generate_allure_report.sh

--- generate_allure_report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "/Users/muhammadsultan/.m2/repository/allure/allure-2.18.1/bin/"
-bash allure serve "$parent_path/allure-results"
-exit
